c32821a9badc6732b6d5380b65d24edab75754c7
HouBinyang <21302010042@m.fudan.edu.cn>
2024-04-08 18:44:47 +0800

    refactor

--- src/TypeCheck.cpp
+++ src/TypeCheck.cpp
@@ -1,30 +1,29 @@
 #include "TypeCheck.h"
 
-//global tabels
-//typeMap func2retType; // function name to return type
+// global tabels
+// typeMap func2retType; // function name to return type
 
 // global token ids to type
-typeMap g_token2Type; 
+typeMap g_token2Type;
 
 // local token ids to type, since func param can override global param
 typeMap funcparam_token2Type;
-vector<typeMap*> local_token2Type;
-
+vector<typeMap *> local_token2Type;
 
 paramMemberMap func2Param;
 paramMemberMap struct2Members;
 
-
 // private util functions
-void error_print(std::ostream& out, A_pos p, string info)
+void error_print(std::ostream &out, A_pos p, string info)
 {
     out << "Typecheck error in line " << p->line << ", col " << p->col << ": " << info << std::endl;
     exit(0);
 }
 
-
-void print_token_map(typeMap* map){
-    for(auto it = map->begin(); it != map->end(); it++){
+void print_token_map(typeMap *map)
+{
+    for (auto it = map->begin(); it != map->end(); it++)
+    {
         std::cout << it->first << " : ";
         switch (it->second->type->type)
         {
@@ -44,101 +43,110 @@ void print_token_map(typeMap* map){
         default:
             break;
         }
-        switch(it->second->isVarArrFunc){
-            case 0:
-                std::cout << " scalar";
-                break;
-            case 1:
-                std::cout << " array";
-                break;
-            case 2:
-                std::cout << " function";
-                break;
+        switch (it->second->isVarArrFunc)
+        {
+        case 0:
+            std::cout << " scalar";
+            break;
+        case 1:
+            std::cout << " array";
+            break;
+        case 2:
+            std::cout << " function";
+            break;
         }
         std::cout << std::endl;
     }
 }
 
-
-void print_token_maps(){
+void print_token_maps()
+{
     std::cout << "global token2Type:" << std::endl;
     print_token_map(&g_token2Type);
     std::cout << "local token2Type:" << std::endl;
     print_token_map(&funcparam_token2Type);
 }
 
-
-bool comp_aA_type(aA_type target, aA_type t){
-    if(!target || !t)
+bool comp_aA_type(aA_type target, aA_type t)
+{
+    if (!target || !t)
         return false;
-    if(target->type != t->type)
+    if (target->type != t->type)
         return false;
-    if(target->type == A_dataType::A_nativeTypeKind)
-        if(target->u.nativeType != t->u.nativeType)
+    if (target->type == A_dataType::A_nativeTypeKind)
+        if (target->u.nativeType != t->u.nativeType)
             return false;
-    if(target->type == A_dataType::A_structTypeKind)
-        if(target->u.structType != t->u.structType)
+    if (target->type == A_dataType::A_structTypeKind)
+        if (target->u.structType != t->u.structType)
             return false;
     return true;
 }
 
-
-bool comp_tc_type(tc_type target, tc_type t){
-    if(!target || !t)
+bool comp_tc_type(tc_type target, tc_type t)
+{
+    if (!target || !t)
         return false;
-    
+
     // arr kind first
     if (target->isVarArrFunc & t->isVarArrFunc == 0)
         return false;
-    
+
     // if target type is nullptr, alwayse ok
     return comp_aA_type(target->type, t->type);
 }
 
-
-tc_type tc_Type(aA_type t, uint isVarArrFunc){
+tc_type tc_Type(aA_type t, uint isVarArrFunc)
+{
     tc_type ret = new tc_type_;
     ret->type = t;
     ret->isVarArrFunc = isVarArrFunc;
     return ret;
 }
 
-
-tc_type tc_Type(aA_varDecl vd){
-    if(vd->kind == A_varDeclType::A_varDeclScalarKind)
+tc_type tc_Type(aA_varDecl vd)
+{
+    if (vd->kind == A_varDeclType::A_varDeclScalarKind)
         return tc_Type(vd->u.declScalar->type, 0);
-    else if(vd->kind == A_varDeclType::A_varDeclArrayKind)
+    else if (vd->kind == A_varDeclType::A_varDeclArrayKind)
         return tc_Type(vd->u.declArray->type, 1);
     return nullptr;
 }
 
-
 // public functions
-void check_Prog(std::ostream& out, aA_program p)
+void check_Prog(std::ostream &out, aA_program p)
 {
     for (auto ele : p->programElements)
     {
-        if(ele->kind == A_programVarDeclStmtKind){
+        if (ele->kind == A_programVarDeclStmtKind)
+        {
             check_VarDecl(out, ele->u.varDeclStmt);
-        }else if (ele->kind == A_programStructDefKind){
+        }
+        else if (ele->kind == A_programStructDefKind)
+        {
             check_StructDef(out, ele->u.structDef);
         }
     }
-    
-    for (auto ele : p->programElements){
-        if(ele->kind == A_programFnDeclStmtKind){
+
+    for (auto ele : p->programElements)
+    {
+        if (ele->kind == A_programFnDeclStmtKind)
+        {
             check_FnDeclStmt(out, ele->u.fnDeclStmt);
         }
-        else if (ele->kind == A_programFnDefKind){
+        else if (ele->kind == A_programFnDefKind)
+        {
             check_FnDecl(out, ele->u.fnDef->fnDecl);
         }
     }
 
-    for (auto ele : p->programElements){
-        if(ele->kind == A_programFnDefKind){
+    for (auto ele : p->programElements)
+    {
+        if (ele->kind == A_programFnDefKind)
+        {
             check_FnDef(out, ele->u.fnDef);
         }
-        else if (ele->kind == A_programNullStmtKind){
+        else if (ele->kind == A_programNullStmtKind)
+        {
             // do nothing
         }
     }
@@ -147,39 +155,76 @@ void check_Prog(std::ostream& out, aA_program p)
     return;
 }
 
-
-void check_VarDecl(std::ostream& out, aA_varDeclStmt vd)
+// 检查全局变量声明和定义
+void check_VarDecl(std::ostream &out, aA_varDeclStmt vd)
 {
     if (!vd)
         return;
     string name;
-    if (vd->kind == A_varDeclStmtType::A_varDeclKind){
+    if (vd->kind == A_varDeclStmtType::A_varDeclKind)
+    {
         // decl only
         aA_varDecl vdecl = vd->u.varDecl;
-        if(vdecl->kind == A_varDeclType::A_varDeclScalarKind){
+        if (vdecl->kind == A_varDeclType::A_varDeclScalarKind)
+        {
             name = *vdecl->u.declScalar->id;
             /* fill code here*/
-        }else if (vdecl->kind == A_varDeclType::A_varDeclArrayKind){
+
+            // 检查是否重复声明
+            if (g_token2Type.find(name) != g_token2Type.end())
+                error_print(out, vdecl->pos, "This id is already defined!");
+
+            // 添加到全局变量表
+            g_token2Type[name] = tc_Type(vdecl);
+        }
+        else if (vdecl->kind == A_varDeclType::A_varDeclArrayKind)
+        {
             name = *vdecl->u.declArray->id;
             /* fill code here*/
-            }
+
+            // 检查是否重复声明
+            if (g_token2Type.find(name) != g_token2Type.end())
+                error_print(out, vdecl->pos, "This id is already defined!");
+
+            // 添加到全局变量表
+            g_token2Type[name] = tc_Type(vdecl);
+
+        }
     }
-    else if (vd->kind == A_varDeclStmtType::A_varDefKind){
+    else if (vd->kind == A_varDeclStmtType::A_varDefKind)
+    {
         // decl and def
         aA_varDef vdef = vd->u.varDef;
-        if (vdef->kind == A_varDefType::A_varDefScalarKind){
+        if (vdef->kind == A_varDefType::A_varDefScalarKind)
+        {
             name = *vdef->u.defScalar->id;
             /* fill code here, allow omited type */
-        }else if (vdef->kind == A_varDefType::A_varDefArrayKind){
+
+            // 检查是否重复声明
+            if (g_token2Type.find(name) != g_token2Type.end())
+                error_print(out, vdef->pos, "This id is already defined!");
+
+            // 添加到全局变量表
+            g_token2Type[name] = tc_Type(vdef->u.defScalar->type, 0);
+        }
+        else if (vdef->kind == A_varDefType::A_varDefArrayKind)
+        {
             name = *vdef->u.defArray->id;
             /* fill code here, allow omited type */
+
+            // 检查是否重复声明
+            if (g_token2Type.find(name) != g_token2Type.end())
+                error_print(out, vdef->pos, "This id is already defined!");
+            
+            // 添加到全局变量表
+            g_token2Type[name] = tc_Type(vdef->u.defArray->type, 1);
         }
     }
     return;
 }
 
-
-void check_StructDef(std::ostream& out, aA_structDef sd)
+// 检查全局结构体定义
+void check_StructDef(std::ostream &out, aA_structDef sd)
 {
     if (!sd)
         return;
@@ -190,28 +235,30 @@ void check_StructDef(std::ostream& out, aA_structDef sd)
     return;
 }
 
-
-void check_FnDecl(std::ostream& out, aA_fnDecl fd)
+// 检查全局函数声明
+void check_FnDecl(std::ostream &out, aA_fnDecl fd)
 {
     if (!fd)
         return;
     string name = *fd->id;
 
     // if already declared, should match
-    if (func2Param.find(name) != func2Param.end()){
+    if (func2Param.find(name) != func2Param.end())
+    {
         // is function ret val matches
         /* fill code here */
         // is function params matches decl
         /* fill code here */
-    }else{
+    }
+    else
+    {
         // if not defined
         /* fill code here */
     }
     return;
 }
 
-
-void check_FnDeclStmt(std::ostream& out, aA_fnDeclStmt fd)
+void check_FnDeclStmt(std::ostream &out, aA_fnDeclStmt fd)
 {
     if (!fd)
         return;
@@ -219,8 +266,8 @@ void check_FnDeclStmt(std::ostream& out, aA_fnDeclStmt fd)
     return;
 }
 
-
-void check_FnDef(std::ostream& out, aA_fnDef fd)
+// 检查全局函数定义
+void check_FnDef(std::ostream &out, aA_fnDef fd)
 {
     if (!fd)
         return;
@@ -237,15 +284,16 @@ void check_FnDef(std::ostream& out, aA_fnDef fd)
     {
         check_CodeblockStmt(out, stmt);
         // return value type should match
-        /* fill code here */        
+        /* fill code here */
     }
 
     return;
 }
 
-
-void check_CodeblockStmt(std::ostream& out, aA_codeBlockStmt cs){
-    if(!cs)
+// 检查局部代码块
+void check_CodeblockStmt(std::ostream &out, aA_codeBlockStmt cs)
+{
+    if (!cs)
         return;
     // variables declared in a code block should not duplicate with outer ones.
     switch (cs->kind)
@@ -274,82 +322,87 @@ void check_CodeblockStmt(std::ostream& out, aA_codeBlockStmt cs){
     return;
 }
 
-
-void check_AssignStmt(std::ostream& out, aA_assignStmt as){
-    if(!as)
+void check_AssignStmt(std::ostream &out, aA_assignStmt as)
+{
+    if (!as)
         return;
     string name;
     tc_type deduced_type; // deduced type if type is omitted at decl
     switch (as->leftVal->kind)
     {
-        case A_leftValType::A_varValKind:{
-            name = *as->leftVal->u.id;
-            /* fill code here */
-        }
-            break;
-        case A_leftValType::A_arrValKind:{
-            name = *as->leftVal->u.arrExpr->arr->u.id;
-            /* fill code here */
-        }
-            break;
-        case A_leftValType::A_memberValKind:{
-            /* fill code here */
-        }
-            break;
+    case A_leftValType::A_varValKind:
+    {
+        name = *as->leftVal->u.id;
+        /* fill code here */
+    }
+    break;
+    case A_leftValType::A_arrValKind:
+    {
+        name = *as->leftVal->u.arrExpr->arr->u.id;
+        /* fill code here */
+    }
+    break;
+    case A_leftValType::A_memberValKind:
+    {
+        /* fill code here */
+    }
+    break;
     }
     return;
 }
 
-
-void check_ArrayExpr(std::ostream& out, aA_arrayExpr ae){
-    if(!ae)
+void check_ArrayExpr(std::ostream &out, aA_arrayExpr ae)
+{
+    if (!ae)
         return;
     string name = *ae->arr->u.id;
     // check array name
     /* fill code here */
-        
+
     // check index
     /* fill code here */
     return;
 }
 
- 
-tc_type check_MemberExpr(std::ostream& out, aA_memberExpr me){
+tc_type check_MemberExpr(std::ostream &out, aA_memberExpr me)
+{
     // check if the member exists and return the tyep of the member
-    if(!me)
+    if (!me)
         return nullptr;
     string name = *me->structId->u.id;
     // check struct name
     /* fill code here */
-        
+
     // check member name
     /* fill code here */
-        
+
     return nullptr;
 }
 
-
-void check_IfStmt(std::ostream& out, aA_ifStmt is){
-    if(!is)
+void check_IfStmt(std::ostream &out, aA_ifStmt is)
+{
+    if (!is)
         return;
     check_BoolExpr(out, is->boolExpr);
     /* fill code here, take care of variable scope */
 
-    for(aA_codeBlockStmt s : is->ifStmts){
+    for (aA_codeBlockStmt s : is->ifStmts)
+    {
         check_CodeblockStmt(out, s);
     }
-    
-    /* fill code here */    
-    for(aA_codeBlockStmt s : is->elseStmts){
+
+    /* fill code here */
+    for (aA_codeBlockStmt s : is->elseStmts)
+    {
         check_CodeblockStmt(out, s);
     }
     /* fill code here */
     return;
 }
 
-
-void check_BoolExpr(std::ostream& out, aA_boolExpr be){
-    if(!be)
+void check_BoolExpr(std::ostream &out, aA_boolExpr be)
+{
+    if (!be)
         return;
     switch (be->kind)
     {
@@ -366,140 +419,149 @@ void check_BoolExpr(std::ostream& out, aA_boolExpr be){
     return;
 }
 
-
-void check_BoolUnit(std::ostream& out, aA_boolUnit bu){
-    if(!bu)
+void check_BoolUnit(std::ostream &out, aA_boolUnit bu)
+{
+    if (!bu)
         return;
     switch (bu->kind)
     {
-        case A_boolUnitType::A_comOpExprKind:{
-            /* fill code here */
-        }
-            break;
-        case A_boolUnitType::A_boolExprKind:
-            check_BoolExpr(out, bu->u.boolExpr);
-            break;
-        case A_boolUnitType::A_boolUOpExprKind:
-            check_BoolUnit(out, bu->u.boolUOpExpr->cond);
-            break;
-        default:
-            break;
+    case A_boolUnitType::A_comOpExprKind:
+    {
+        /* fill code here */
+    }
+    break;
+    case A_boolUnitType::A_boolExprKind:
+        check_BoolExpr(out, bu->u.boolExpr);
+        break;
+    case A_boolUnitType::A_boolUOpExprKind:
+        check_BoolUnit(out, bu->u.boolUOpExpr->cond);
+        break;
+    default:
+        break;
     }
     return;
 }
 
-
-tc_type check_ExprUnit(std::ostream& out, aA_exprUnit eu){
+tc_type check_ExprUnit(std::ostream &out, aA_exprUnit eu)
+{
     // return the aA_type of expr eu
-    if(!eu)
+    if (!eu)
         return nullptr;
     tc_type ret;
     switch (eu->kind)
     {
-        case A_exprUnitType::A_idExprKind:{
-            /* fill code here */
-        }
-            break;
-        case A_exprUnitType::A_numExprKind:{
-            aA_type numt = new aA_type_;
-            numt->pos = eu->pos;
-            numt->type = A_dataType::A_nativeTypeKind;
-            numt->u.nativeType = A_nativeType::A_intTypeKind;
-            ret = tc_Type(numt, 0);
-        }
-            break;
-        case A_exprUnitType::A_fnCallKind:{
-            check_FuncCall(out, eu->u.callExpr);
-            // check_FuncCall will check if the function is defined
-            /* fill code here */
-        }
-            break;
-        case A_exprUnitType::A_arrayExprKind:{
-            check_ArrayExpr(out, eu->u.arrayExpr);
-            /* fill code here */
-        }
-            break;
-        case A_exprUnitType::A_memberExprKind:{
-            ret = check_MemberExpr(out, eu->u.memberExpr);
-        }
-            break;
-        case A_exprUnitType::A_arithExprKind:{
-            ret = check_ArithExpr(out, eu->u.arithExpr);
-        }
-            break;
-        case A_exprUnitType::A_arithUExprKind:{
-            ret = check_ExprUnit(out, eu->u.arithUExpr->expr);
-        }
-            break;
+    case A_exprUnitType::A_idExprKind:
+    {
+        /* fill code here */
+    }
+    break;
+    case A_exprUnitType::A_numExprKind:
+    {
+        aA_type numt = new aA_type_;
+        numt->pos = eu->pos;
+        numt->type = A_dataType::A_nativeTypeKind;
+        numt->u.nativeType = A_nativeType::A_intTypeKind;
+        ret = tc_Type(numt, 0);
+    }
+    break;
+    case A_exprUnitType::A_fnCallKind:
+    {
+        check_FuncCall(out, eu->u.callExpr);
+        // check_FuncCall will check if the function is defined
+        /* fill code here */
+    }
+    break;
+    case A_exprUnitType::A_arrayExprKind:
+    {
+        check_ArrayExpr(out, eu->u.arrayExpr);
+        /* fill code here */
+    }
+    break;
+    case A_exprUnitType::A_memberExprKind:
+    {
+        ret = check_MemberExpr(out, eu->u.memberExpr);
+    }
+    break;
+    case A_exprUnitType::A_arithExprKind:
+    {
+        ret = check_ArithExpr(out, eu->u.arithExpr);
+    }
+    break;
+    case A_exprUnitType::A_arithUExprKind:
+    {
+        ret = check_ExprUnit(out, eu->u.arithUExpr->expr);
+    }
+    break;
     }
     return ret;
 }
 
-
-tc_type check_ArithExpr(std::ostream& out, aA_arithExpr ae){
-    if(!ae)
+tc_type check_ArithExpr(std::ostream &out, aA_arithExpr ae)
+{
+    if (!ae)
         return nullptr;
     tc_type ret;
     switch (ae->kind)
     {
-        case A_arithExprType::A_arithBiOpExprKind:{
-            ret = check_ArithExpr(out, ae->u.arithBiOpExpr->left);
-            tc_type rightTyep = check_ArithExpr(out, ae->u.arithBiOpExpr->right);
-            if(ret->type->type > 0 || ret->type->type != A_dataType::A_nativeTypeKind || ret->type->u.nativeType != A_nativeType::A_intTypeKind ||
+    case A_arithExprType::A_arithBiOpExprKind:
+    {
+        ret = check_ArithExpr(out, ae->u.arithBiOpExpr->left);
+        tc_type rightTyep = check_ArithExpr(out, ae->u.arithBiOpExpr->right);
+        if (ret->type->type > 0 || ret->type->type != A_dataType::A_nativeTypeKind || ret->type->u.nativeType != A_nativeType::A_intTypeKind ||
             rightTyep->type->type > 0 || rightTyep->type->type != A_dataType::A_nativeTypeKind || rightTyep->type->u.nativeType != A_nativeType::A_intTypeKind)
-                error_print(out, ae->pos, "Only int can be arithmetic expression operation values!");
-        }
-            break;
-        case A_arithExprType::A_exprUnitKind:
-            ret = check_ExprUnit(out, ae->u.exprUnit);
-            break;
+            error_print(out, ae->pos, "Only int can be arithmetic expression operation values!");
+    }
+    break;
+    case A_arithExprType::A_exprUnitKind:
+        ret = check_ExprUnit(out, ae->u.exprUnit);
+        break;
     }
     return ret;
 }
 
-
-void check_FuncCall(std::ostream& out, aA_fnCall fc){
-    if(!fc)
+void check_FuncCall(std::ostream &out, aA_fnCall fc)
+{
+    if (!fc)
         return;
     // check if function defined
     string func_name = *fc->fn;
     /* fill code here */
-        
+
     // check if parameter list matches
-    for(int i = 0; i < fc->vals.size(); i++){
+    for (int i = 0; i < fc->vals.size(); i++)
+    {
         /* fill code here */
-        
     }
-    return ;
-}
-
-
-void check_WhileStmt(std::ostream& out, aA_whileStmt ws){
-    if(!ws)
-        return;
-    check_BoolExpr(out, ws->boolExpr);
-    /* fill code here, take care of variable scope */
-        
-    for(aA_codeBlockStmt s : ws->whileStmts){
-        check_CodeblockStmt(out, s);
-    }
-    /* fill code here */
-        
     return;
 }
 
+void check_WhileStmt(std::ostream &out, aA_whileStmt ws)
+{
+    if (!ws)
+        return;
+    check_BoolExpr(out, ws->boolExpr);
+    /* fill code here, take care of variable scope */
 
-void check_CallStmt(std::ostream& out, aA_callStmt cs){
-    if(!cs)
+    for (aA_codeBlockStmt s : ws->whileStmts)
+    {
+        check_CodeblockStmt(out, s);
+    }
+    /* fill code here */
+
+    return;
+}
+
+void check_CallStmt(std::ostream &out, aA_callStmt cs)
+{
+    if (!cs)
         return;
     check_FuncCall(out, cs->fnCall);
     return;
 }
 
-
-void check_ReturnStmt(std::ostream& out, aA_returnStmt rs){
-    if(!rs)
+void check_ReturnStmt(std::ostream &out, aA_returnStmt rs)
+{
+    if (!rs)
         return;
     return;
 }
-
