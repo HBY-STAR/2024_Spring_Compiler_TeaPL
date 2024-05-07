c3b06494be4f36c87c52390b1da4d4e074839b1a
HouBinyang <21302010042@m.fudan.edu.cn>
2024-05-07 11:06:25 +0800

    feat: dominator

--- src/ssa.cpp
+++ src/ssa.cpp
@@ -7,6 +7,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <algorithm>
 #include "bg_llvm.h"
 #include "graph.hpp"
 #include "liveness.h"
@@ -58,14 +59,16 @@ LLVMIR::L_prog *SSA(LLVMIR::L_prog *prog)
         // Show_graph(stdout,RA_bg);
         SingleSourceGraph(RA_bg.mynodes[0], RA_bg, fun);
         // cout << endl;
-        Show_graph(stdout,RA_bg);
+        Show_graph(stdout, RA_bg);
 
         Liveness(RA_bg.mynodes[0], RA_bg, fun->args);
-        Show_Liveness(stdout, RA_bg);
-        exit(0);
+        // Show_Liveness(stdout, RA_bg);
+        // exit(0);
 
         Dominators(RA_bg);
-        // printf_domi();
+        printf_domi();
+        exit(0);
+
         tree_Dominators(RA_bg);
         // printf_D_tree();
         // 默认0是入口block
@@ -229,14 +232,112 @@ void mem2reg(LLVMIR::L_func *fun)
 // Todo
 void Dominators(GRAPH::Graph<LLVMIR::L_block *> &bg)
 {
-    // 计算支配节点
+    // D[s0] = {s0}
+    // D[n] = {n}∪ ( ⋂(D[p]) ) p∈pred(n),  for n∈V-{s0}
 
-    // 初始化
+    // 初始化，由于赋值使D[n]变小，所以初始化为全集
+    std::unordered_set<LLVMIR::L_block *> all_blocks;
     for (auto x : bg.mynodes)
     {
-        dominators[x.second->nodeInfo()] = unordered_set<L_block *>();
+        all_blocks.insert(x.second->nodeInfo());
     }
-    
+    for (auto x : bg.mynodes)
+    {
+        dominators[x.second->nodeInfo()] = all_blocks;
+    }
+
+    // D[s0] = {s0}
+    dominators[bg.mynodes[0]->nodeInfo()].clear();
+    dominators[bg.mynodes[0]->nodeInfo()].insert(bg.mynodes[0]->nodeInfo());
+
+    printf_domi();
+
+    // 迭代计算
+    bool change = true;
+    while (change)
+    {
+        change = false;
+        for (auto x : bg.mynodes)
+        {
+            if (x.first == 0)
+            {
+                continue;
+            }
+
+            // D[n] = {n}∪ ( ⋂(D[p]) ) p∈pred(n),  for n∈V-{s0}
+
+            // ⋂(D[p])
+            std::unordered_set<LLVMIR::L_block *> new_dominators;
+            bool first_flag = true;
+            for (auto p : x.second->preds)
+            {
+                if(first_flag)
+                {
+                    first_flag = false;
+                    new_dominators = dominators[bg.mynodes[p]->nodeInfo()];
+                }
+                else
+                {
+                    new_dominators = Intersect(new_dominators, dominators[bg.mynodes[p]->nodeInfo()]);
+                }
+            }
+
+            // {n}∪ ( ⋂(D[p]) )
+            new_dominators.insert(x.second->nodeInfo());
+
+            if (!Set_eq(new_dominators, dominators[x.second->nodeInfo()]))
+            {
+                change = true;
+                dominators[x.second->nodeInfo()] = new_dominators;
+            }
+        }
+    }
+}
+
+// 对无序集合求交集
+std::unordered_set<LLVMIR::L_block *> Intersect(std::unordered_set<LLVMIR::L_block *> &a, std::unordered_set<LLVMIR::L_block *> &b)
+{
+    unordered_set<LLVMIR::L_block *> ret;
+    for (auto x : a)
+    {
+        if (b.find(x) != b.end())
+        {
+            ret.insert(x);
+        }
+    }
+    return ret;
+}
+
+// 对无序集合求并集
+std::unordered_set<LLVMIR::L_block *> Union(std::unordered_set<LLVMIR::L_block *> &a, std::unordered_set<LLVMIR::L_block *> &b)
+{
+    unordered_set<LLVMIR::L_block *> ret;
+    for (auto x : a)
+    {
+        ret.insert(x);
+    }
+    for (auto x : b)
+    {
+        ret.insert(x);
+    }
+    return ret;
+}
+
+// 求无序集合是否相等
+bool Set_eq(std::unordered_set<LLVMIR::L_block *> &a, std::unordered_set<LLVMIR::L_block *> &b)
+{
+    if (a.size() != b.size())
+    {
+        return false;
+    }
+    for (auto x : a)
+    {
+        if (b.find(x) == b.end())
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 void printf_domi()
